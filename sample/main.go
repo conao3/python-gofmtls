d71f0edfe0438e1b18ec19918909fc9d3455c42c
conao3 <conao3@gmail.com>
2025-01-07 00:43:30 +0900

    add sample

---
+++ sample/main.go
@@ -0,0 +1,21 @@
+package main
+import(
+"strings"
+"fmt"
+)
+type T struct{
+name string // オブジェクトの名前
+value int // その値
+}
+func main(){
+sum := 0
+for i:=1;i<=10;i++{
+if (i > 5) {
+s := "iが6以上です"
+fmt.Printf("%d\n", s)
+fmt.Printf("%v\n", strings.Fields(s))
+}
+sum += i
+}
+fmt.Printf("%d\n", sum)
+}
